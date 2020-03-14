7851b0687b8d40bafc4bf75cccd4ad0ec86e4af5
nesrinesghaier <nesrinesghaier10@gmail.com>
2020-03-15 00:12:58 +0100

    fixes

--- parseRoutes.js
+++ parseRoutes.js
@@ -6,12 +6,13 @@ import data from './app/data/routes';
  * creates index.html files for all routes in ./dist
  */
 
-data.filter((route) => !route.index).forEach((route) => {
-  const subdir = route.path.substr(1);
-  const dir = path.join(__dirname, 'dist', subdir);
-  if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir);
-  }
-  fs.createReadStream(path.join(__dirname, 'dist/index.html'))
-    .pipe(fs.createWriteStream(path.join(dir, 'index.html')));
-});
+data.filter((route) => !route.index)
+  .forEach((route) => {
+    const subdir = route.path.substr(1);
+    const dir = path.join(__dirname, 'dist', subdir);
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+    }
+    fs.createReadStream(path.join(__dirname, 'dist/index.html'))
+      .pipe(fs.createWriteStream(path.join(dir, 'index.html')));
+  });
