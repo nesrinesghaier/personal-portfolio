d506eaef829c6544e7b0a3f71e1046506fcb4707
nesrinesghaier <nesrinesghaier10@gmail.com>
2020-03-14 16:46:32 +0100

    init commit

---
+++ app/components/Template/Nav.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
+import data from '../../data/contact';
+
+const Nav = () => (
+  <section id="sidebar">
+    <section id="intro">
+      <Link to="/" className="logo">
+        <img src={`${BASE_PATH}/images/NESRINE.jpg`} alt="" />
+      </Link>
+      <header>
+        <h2>Nesrine Sghaier</h2>
+        <p><a href="mailto:nesrinesghaier10@gmail.com">nesrinesghaier10@gmail.com</a></p>
+      </header>
+    </section>
+
+    <section className="blurb">
+      <h2>Summary</h2>
+      <p>Hi, I&apos;m Nesrine Sghaier. I am an ENISo (National Engineering School of Sousse) Alumni
+        and a Full Stack Engineer currently. Most of the time, I am driven by curiosity and
+        competitiveness.
+      </p>
+      <ul className="actions">
+        <li>
+          {window.location.pathname !== `${BASE_PATH}/resume`
+            ? <Link to="/resume" className="button">Learn More</Link>
+            : <Link to="/about" className="button">About Me</Link>}
+        </li>
+      </ul>
+    </section>
+
+    <section id="footer">
+      <ul className="icons">
+        {data.map((s) => (
+          <li key={s.label}>
+            <a href={s.link}>
+              <FontAwesomeIcon icon={s.icon} />
+            </a>
+          </li>
+        ))}
+      </ul>
+      <p className="copyright">&copy; Nesrine Sghaier.</p>
+    </section>
+  </section>
+);
+
+export default Nav;
---
+++ app/data/contact.js
@@ -0,0 +1,47 @@
+import faGithub from '@fortawesome/fontawesome-free-brands/faGithub';
+import faFacebook from '@fortawesome/fontawesome-free-brands/faFacebookF';
+import faInstagram from '@fortawesome/fontawesome-free-brands/faInstagram';
+import faLinkedinIn from '@fortawesome/fontawesome-free-brands/faLinkedinIn';
+import faAngellist from '@fortawesome/fontawesome-free-brands/faAngellist';
+import faTwitter from '@fortawesome/fontawesome-free-brands/faTwitter';
+import faEnvelope from '@fortawesome/fontawesome-free-regular/faEnvelope';
+
+const data = [
+  {
+    link: 'https://github.com/nesrinesghaier',
+    label: 'Github',
+    icon: faGithub,
+  },
+  {
+    link: 'https://www.facebook.com/nesrine.sghaier10',
+    label: 'Facebook',
+    icon: faFacebook,
+  },
+  {
+    link: 'https://www.instagram.com/sghaier_nesrine/?hl=en',
+    label: 'Instagram',
+    icon: faInstagram,
+  },
+  {
+    link: 'https://www.linkedin.com/in/sghaiernesrine/',
+    label: 'LinkedIn',
+    icon: faLinkedinIn,
+  },
+  {
+    link: 'https://angel.co/nesrine-sghaier',
+    label: 'Angel List',
+    icon: faAngellist,
+  },
+  {
+    link: 'https://twitter.com/nesrine_sghaier',
+    label: 'Twitter',
+    icon: faTwitter,
+  },
+  {
+    link: 'mailto:nesrinesghaier10@gmail.com',
+    label: 'Email',
+    icon: faEnvelope,
+  },
+];
+
+export default data;
